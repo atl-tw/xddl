1e25f01ee352fa354be258206dc5e9a1635f52f1
kebernet <rcooper@thoughtworks.com>
2019-07-24 18:48:08 -0400

    Reflection cleanup

--- j2xddl/src/main/java/net/kebernet/xddl/j2xddl/Main.java
+++ j2xddl/src/main/java/net/kebernet/xddl/j2xddl/Main.java
@@ -15,19 +15,18 @@
  */
 package net.kebernet.xddl.j2xddl;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.ParameterException;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Enumeration;
-import java.util.HashSet;
-import java.util.List;
+
+import static net.kebernet.xddl.j2xddl.Reflection.findClasses;
 
 public class Main {
 
@@ -57,50 +56,15 @@ public class Main {
     }
   }
 
-  public void run(List<String> packageNames, File outputFile)
+  void run(List<String> packageNames, File outputFile)
       throws IOException, ClassNotFoundException {
     HashSet<Class> classes = new HashSet<>();
     for (String s : packageNames) {
-      classes.addAll(Arrays.asList(getClasses(s)));
+      classes.addAll(findClasses(s));
     }
     Generator generator = new Generator(mapper, classes);
     mapper.writeValue(outputFile, generator.generate());
   }
 
-  private static Class[] getClasses(String packageName) throws ClassNotFoundException, IOException {
-    ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-    assert classLoader != null;
-    String path = packageName.replace('.', '/');
-    Enumeration<URL> resources = classLoader.getResources(path);
-    List<File> dirs = new ArrayList<>();
-    while (resources.hasMoreElements()) {
-      URL resource = resources.nextElement();
-      dirs.add(new File(resource.getFile()));
-    }
-    ArrayList classes = new ArrayList();
-    for (File directory : dirs) {
-      classes.addAll(findClasses(directory, packageName));
-    }
-    return (Class[]) classes.toArray(new Class[classes.size()]);
-  }
 
-  private static List<Class> findClasses(File directory, String packageName)
-      throws ClassNotFoundException {
-    List<Class> classes = new ArrayList<>();
-    if (!directory.exists()) {
-      return classes;
-    }
-    File[] files = directory.listFiles();
-    for (File file : files == null ? new File[0] : files) {
-      if (file.isDirectory()) {
-        assert !file.getName().contains(".");
-        classes.addAll(findClasses(file, packageName + "." + file.getName()));
-      } else if (file.getName().endsWith(".class")) {
-        classes.add(
-            Class.forName(
-                packageName + '.' + file.getName().substring(0, file.getName().length() - 6)));
-      }
-    }
-    return classes;
-  }
 }
--- j2xddl/src/main/java/net/kebernet/xddl/j2xddl/Reflection.java
+++ j2xddl/src/main/java/net/kebernet/xddl/j2xddl/Reflection.java
@@ -16,26 +16,31 @@
 package net.kebernet.xddl.j2xddl;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.io.File;
+import java.io.IOException;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Enumeration;
 import java.util.List;
 
-public abstract class Reflection {
+abstract class Reflection {
 
   private Reflection() {}
 
-  public static boolean hasAnyOf(AnnotatedElement element, Class<? extends Annotation>... type) {
+  static boolean hasAnyOf(AnnotatedElement element, Class<? extends Annotation>... type) {
     return Arrays.stream(type).anyMatch(element::isAnnotationPresent);
   }
 
-  public static List<Field> listFields(Class clazz) {
+  static List<Field> listFields(Class clazz) {
     List<Field> result = new ArrayList<>(clazz.getDeclaredFields().length);
     Arrays.stream(clazz.getDeclaredFields())
         .filter(f -> !Modifier.isStatic(f.getModifiers()) && !Modifier.isFinal(f.getModifiers()))
@@ -44,12 +49,49 @@ public abstract class Reflection {
     return result;
   }
 
-  public static Type nestedType(Field f) {
+  static Type nestedType(Field f) {
     if (Collection.class.isAssignableFrom(f.getType())) {
       ParameterizedType collectionType = (ParameterizedType) f.getGenericType();
       return collectionType.getActualTypeArguments()[0];
     } else {
       return f.getType();
     }
+  }
+
+  static List<Class> findClasses(String packageName) throws ClassNotFoundException, IOException {
+    ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+    assert classLoader != null;
+    String path = packageName.replace('.', '/');
+    Enumeration<URL> resources = classLoader.getResources(path);
+    List<File> dirs = new ArrayList<>();
+    while (resources.hasMoreElements()) {
+      URL resource = resources.nextElement();
+      dirs.add(new File(resource.getFile()));
+    }
+    ArrayList<Class> classes = new ArrayList<>();
+    for (File directory : dirs) {
+      classes.addAll(findClasses(directory, packageName));
+    }
+    return classes;
+  }
+
+  static List<Class> findClasses(File directory, String packageName)
+          throws ClassNotFoundException {
+    List<Class> classes = new ArrayList<>();
+    if (!directory.exists()) {
+      return classes;
+    }
+    File[] files = directory.listFiles();
+    for (File file : files == null ? new File[0] : files) {
+      if (file.isDirectory()) {
+        assert !file.getName().contains(".");
+        classes.addAll(findClasses(file, packageName + "." + file.getName()));
+      } else if (file.getName().endsWith(".class")) {
+        classes.add(
+                Class.forName(
+                        packageName + '.' + file.getName().substring(0, file.getName().length() - 6)));
+      }
+    }
+    return classes;
   }
 }
