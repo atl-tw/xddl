b17ad89b1198ff573243bdde47e2c5379f59e5d6
kebernet <rcooper@thoughtworks.com>
2019-08-05 13:29:19 -0400

    Initial work on the Java generator

--- java/src/main/java/net/kebernet/xddl/java/JavaPlugin.java
+++ java/src/main/java/net/kebernet/xddl/java/JavaPlugin.java
@@ -1,4 +1,19 @@
 package net.kebernet.xddl.java;
 
-public class JavaPlugin {
+import java.io.File;
+import java.io.IOException;
+
+import net.kebernet.xddl.plugins.Context;
+import net.kebernet.xddl.plugins.Plugin;
+
+public class JavaPlugin implements Plugin {
+    @Override
+    public String getName() {
+        return "java";
+    }
+
+    @Override
+    public String generateArtifacts(Context context, File outputDirectory) throws IOException {
+        return null;
+    }
 }
