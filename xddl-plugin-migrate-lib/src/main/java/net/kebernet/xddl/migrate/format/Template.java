25bb016c2f0663855af8a7d3c340f6099216fc34
kebernet <rcooper@thoughtworks.com>
2019-11-21 14:24:38 -0500

    Adding insert into template.

---
+++ xddl-plugin-migrate-lib/src/main/java/net/kebernet/xddl/migrate/format/Template.java
@@ -0,0 +1,4 @@
+package net.kebernet.xddl.migrate.format;
+
+public class Template {
+}
---
+++ xddl-plugin-migrate-lib/src/test/java/net/kebernet/xddl/migrate/format/TemplateTest.java
@@ -0,0 +1,7 @@
+package net.kebernet.xddl.migrate.format;
+
+import static org.junit.Assert.*;
+
+public class TemplateTest {
+
+}
